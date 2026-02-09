317621a12dc3315646a5e35a4f86e173fbe28cae
CharlesMwaura <94879757+cmwrxh@users.noreply.github.com>
2026-02-09 14:18:59 +0300

    Update trace_route.py

--- docs/docs/docs/docs/docs/docs/labs/wireshark/labs/wireshark/captures/labs/wireshark/screenshots/labs/curl-openssl/labs/curl-openssl/labs/cdn-latency/labs/cdn-latency/scripts/trace_route.py
+++ docs/docs/docs/docs/docs/docs/labs/wireshark/labs/wireshark/captures/labs/wireshark/screenshots/labs/curl-openssl/labs/curl-openssl/labs/cdn-latency/labs/cdn-latency/scripts/trace_route.py
@@ -1,1 +1,15 @@
+import subprocess
+import sys
+
+def traceroute(host):
+    try:
+        subprocess.run(["tracert", host])
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python trace_route.py <host>")
+    else:
+        traceroute(sys.argv[1])
 
